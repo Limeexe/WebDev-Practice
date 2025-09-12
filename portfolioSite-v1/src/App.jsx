7cffe670d84441a94e76628df18510aa57f3c70d
Limeexe <johnlouieabenir@gmail.com>
2025-09-12 19:40:22 +0800

    Modified scrollbar

--- portfolioSite-v1/src/App.jsx
+++ portfolioSite-v1/src/App.jsx
@@ -11,20 +11,24 @@ import Header from "./components/Header";
 const App = () => {
     return (
         <header className="fixed top-0 left-0 w-full h-20 flex-items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
-            <div className="max-w-screen-2xl w-full mx-auto px-4">
+            <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
                 <h1>
                     <a href="/" className="logo">
                         <img src="/images/logo.svg" width={40} height={40} alt="JLAbe" />
                     </a>
                 </h1>
 
-                <div className="">
-                    <button className="" onClick={null}>
+                <div className="relative md:justify-self-center">
+                    <button className="menu-btn md:hidden" onClick={null}>
                         <span className="material-symbols-rounded">
                             menu
                         </span>
                     </button>
+
+                    Navbar
                 </div>
+
+                <a href="contact" className="">Contact Me</a>
             </div>
         </header>
     )
